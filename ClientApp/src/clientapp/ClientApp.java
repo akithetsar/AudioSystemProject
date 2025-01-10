71ead5f7ffe0e57bafb5394643f15456a974140f
Aleksandar Avramovic <aleksandar03avramovic@gmail.com>
2025-01-10 16:16:49 +0100

    Started on client gui

--- ClientApp/src/clientapp/ClientApp.java
+++ ClientApp/src/clientapp/ClientApp.java
@@ -1,196 +1,134 @@
 package clientapp;
+
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ClientApp extends Application {
 
+    // Enum to represent the content views
+    private enum ContentType {
+        ADMIN("Admin Content"),
+        USER("User Content"),
+        TRACKS("Tracks Content");
+
+        private final String displayText;
+
+        ContentType(String displayText) {
+            this.displayText = displayText;
+        }
+
+        public String getDisplayText() {
+            return displayText;
+        }
+    }
+
+    // Instance variable to store the body layout
+    private VBox body;
+
     @Override
     public void start(Stage primaryStage) {
-        // Main Tab Pane
-        TabPane tabPane = new TabPane();
+        // Create Header Section
+        HBox header = createHeader();
 
-        // Users Tab
-        Tab usersTab = new Tab("Users", createUsersTab());
+        // Create Body Section and store it in the instance variable
+        body = createBody();
 
-        // Audio Management Tab
-        Tab audioTab = new Tab("Audio Management", createAudioTab());
+        // Combine in BorderPane
+        BorderPane layout = new BorderPane();
+        layout.setTop(header);  // Header at the top
+        layout.setCenter(body); // Body in the center
 
-        // Categories Tab
-        Tab categoriesTab = new Tab("Categories", createCategoriesTab());
-
-        // Subscriptions Tab
-        Tab subscriptionsTab = new Tab("Subscriptions", createSubscriptionsTab());
-
-        // Listening & Favorites Tab
-        Tab listeningTab = new Tab("Listening & Favorites", createListeningTab());
-
-        // Ratings Tab
-        Tab ratingsTab = new Tab("Ratings", createRatingsTab());
-
-        // Packages Tab
-        Tab packagesTab = new Tab("Packages", createPackagesTab());
-
-        // Add Tabs to TabPane
-        tabPane.getTabs().addAll(usersTab, audioTab, categoriesTab, subscriptionsTab, listeningTab, ratingsTab, packagesTab);
-
-        // Root Scene
-        Scene scene = new Scene(tabPane, 800, 600);
-        primaryStage.setTitle("Audio System GUI");
+        // Scene and Stage
+        Scene scene = new Scene(layout, 800, 600);
+        scene.getStylesheets().add(getClass().getResource("/resources/style.css").toExternalForm());
+        primaryStage.setTitle("Barebones JavaFX GUI");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private VBox createUsersTab() {
-        VBox vbox = new VBox(10);
-        vbox.setPadding(new Insets(10));
+    private HBox createHeader() {
+        HBox header = new HBox(10); // Horizontal layout for header
+        header.getStyleClass().add("header");
 
-        TextField nameField = new TextField();
-        nameField.setPromptText("Name");
+        // Create a StackPane to center the title
+        StackPane titleContainer = createTitleContainer();
 
-        TextField emailField = new TextField();
-        emailField.setPromptText("Email");
+        // Create button container and buttons
+        HBox buttonContainer = createButtonContainer();
 
-        TextField ageField = new TextField();
-        ageField.setPromptText("Age");
+        // Align buttons to the right
+        HBox.setHgrow(titleContainer, Priority.ALWAYS); // Allow title container to grow and push buttons to the right
 
-        ComboBox<String> genderCombo = new ComboBox<>();
-        genderCombo.getItems().addAll("Male", "Female", "Other");
-        genderCombo.setPromptText("Gender");
-
-        TextField locationField = new TextField();
-        locationField.setPromptText("Location");
-
-        Button addButton = new Button("Add User");
-        Button updateButton = new Button("Update User");
-
-        TableView<String> usersTable = new TableView<>();
-
-        vbox.getChildren().addAll(nameField, emailField, ageField, genderCombo, locationField, addButton, updateButton, usersTable);
-        return vbox;
+        // Add the title container and button container to the header
+        header.getChildren().addAll(titleContainer, buttonContainer);
+        return header;
     }
 
-    private VBox createAudioTab() {
-        VBox vbox = new VBox(10);
-        vbox.setPadding(new Insets(10));
-
-        TextField nameField = new TextField();
-        nameField.setPromptText("Audio Name");
-
-        TextField durationField = new TextField();
-        durationField.setPromptText("Duration (seconds)");
-
-        DatePicker uploadDatePicker = new DatePicker();
-
-        Button addAudioButton = new Button("Add Audio");
-        Button editAudioButton = new Button("Edit Audio");
-
-        TableView<String> audioTable = new TableView<>();
-
-        vbox.getChildren().addAll(nameField, durationField, uploadDatePicker, addAudioButton, editAudioButton, audioTable);
-        return vbox;
+    private StackPane createTitleContainer() {
+        StackPane titleContainer = new StackPane();
+        Label title = new Label("My App Header");
+        title.getStyleClass().add("header-title");
+        titleContainer.getChildren().add(title);
+        titleContainer.setMaxWidth(Double.MAX_VALUE); // Allow it to expand fully
+        return titleContainer;
     }
 
-    private VBox createCategoriesTab() {
-        VBox vbox = new VBox(10);
-        vbox.setPadding(new Insets(10));
+    private HBox createButtonContainer() {
+        HBox buttonContainer = new HBox(10); // Add spacing of 10px between buttons
+        buttonContainer.getStyleClass().add("button-container");
 
-        TextField categoryNameField = new TextField();
-        categoryNameField.setPromptText("Category Name");
+        // Create buttons with images
+        Button adminButton = createButton("/static/img/admin.png");
+        Button userButton = createButton("/static/img/user.png");
+        Button tracksButton = createButton("/static/img/tracks.png");
 
-        Button addCategoryButton = new Button("Add Category");
+        // Add event listeners to buttons
+        adminButton.setOnAction(event -> updateBodyContent(ContentType.ADMIN));
+        userButton.setOnAction(event -> updateBodyContent(ContentType.USER));
+        tracksButton.setOnAction(event -> updateBodyContent(ContentType.TRACKS));
 
-        TableView<String> categoriesTable = new TableView<>();
-
-        vbox.getChildren().addAll(categoryNameField, addCategoryButton, categoriesTable);
-        return vbox;
+        // Add buttons to the container
+        buttonContainer.getChildren().addAll(adminButton, userButton, tracksButton);
+        return buttonContainer;
     }
 
-    private VBox createSubscriptionsTab() {
-        VBox vbox = new VBox(10);
-        vbox.setPadding(new Insets(10));
-
-        ComboBox<String> userCombo = new ComboBox<>();
-        userCombo.setPromptText("Select User");
-
-        ComboBox<String> packageCombo = new ComboBox<>();
-        packageCombo.setPromptText("Select Package");
-
-        Button subscribeButton = new Button("Subscribe");
-
-        TableView<String> subscriptionsTable = new TableView<>();
-
-        vbox.getChildren().addAll(userCombo, packageCombo, subscribeButton, subscriptionsTable);
-        return vbox;
+    private Button createButton(String imagePath) {
+        Image image = new Image(getClass().getResource(imagePath).toExternalForm());
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(16);
+        imageView.setFitHeight(16);
+        return new Button("", imageView); // Empty text, image only
     }
 
-    private VBox createListeningTab() {
-        VBox vbox = new VBox(10);
-        vbox.setPadding(new Insets(10));
+    private VBox createBody() {
+        VBox body = new VBox(10); // Vertical layout for body
+        body.getStyleClass().add("body");
 
-        ComboBox<String> userCombo = new ComboBox<>();
-        userCombo.setPromptText("Select User");
+        // Default content
+        Label content = new Label(ContentType.ADMIN.getDisplayText());
+        content.getStyleClass().add("body-content");
+        body.getChildren().add(content);
 
-        ComboBox<String> audioCombo = new ComboBox<>();
-        audioCombo.setPromptText("Select Audio");
-
-        TextField startField = new TextField();
-        startField.setPromptText("Start Second");
-
-        TextField durationField = new TextField();
-        durationField.setPromptText("Duration (seconds)");
-
-        Button logListeningButton = new Button("Log Listening");
-
-        ListView<String> favoritesList = new ListView<>();
-
-        vbox.getChildren().addAll(userCombo, audioCombo, startField, durationField, logListeningButton, favoritesList);
-        return vbox;
+        return body;
     }
 
-    private VBox createRatingsTab() {
-        VBox vbox = new VBox(10);
-        vbox.setPadding(new Insets(10));
+    private void updateBodyContent(ContentType contentType) {
+        // This method is responsible for switching the body content based on the selected button
+        Label contentLabel = new Label(contentType.getDisplayText());
+        contentLabel.getStyleClass().add("body-content");
 
-        ComboBox<String> userCombo = new ComboBox<>();
-        userCombo.setPromptText("Select User");
-
-        ComboBox<String> audioCombo = new ComboBox<>();
-        audioCombo.setPromptText("Select Audio");
-
-        Slider ratingSlider = new Slider(1, 5, 3);
-        ratingSlider.setShowTickLabels(true);
-        ratingSlider.setShowTickMarks(true);
-
-        Button addRatingButton = new Button("Add Rating");
-        Button updateRatingButton = new Button("Update Rating");
-        Button deleteRatingButton = new Button("Delete Rating");
-
-        TableView<String> ratingsTable = new TableView<>();
-
-        vbox.getChildren().addAll(userCombo, audioCombo, ratingSlider, addRatingButton, updateRatingButton, deleteRatingButton, ratingsTable);
-        return vbox;
-    }
-
-    private VBox createPackagesTab() {
-        VBox vbox = new VBox(10);
-        vbox.setPadding(new Insets(10));
-
-        TextField packageNameField = new TextField();
-        packageNameField.setPromptText("Package Name");
-
-        TextField priceField = new TextField();
-        priceField.setPromptText("Price");
-
-        Button addPackageButton = new Button("Add Package");
-
-        TableView<String> packagesTable = new TableView<>();
-
-        vbox.getChildren().addAll(packageNameField, priceField, addPackageButton, packagesTable);
-        return vbox;
+        // Update the body layout by replacing existing content
+        body.getChildren().setAll(contentLabel); // Replace existing content with new content
     }
 
     public static void main(String[] args) {
